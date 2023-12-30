13ff32ba9a7b9799352d279be2e9c81226007900
MahmoudAbdelsamie <msomaa38@gmail.com>
2023-12-30 16:50:49 +0200

    Add Initial Messages Data

---
+++ app.js
@@ -0,0 +1,7 @@
+const messages = {
+    set1: ["You are stronger than you think.", "Believe in yourself.", "Dream big, work hard."],
+    set2: ["Every day is a new opportunity.", "Embrace the journey.", "Stay positive and focused."],
+    set3: ["Success is a mindset.", "Challenges make you stronger.", "You are capable of amazing things."]
+    // To Add more Messages with different sets
+};
+
